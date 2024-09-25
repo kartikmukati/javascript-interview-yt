eb9e4512668897a665cdf8c8a7f3cac19b354704
kartikmukati <kartikmukati2021@gmail.com>
2024-09-26 01:55:33 +0530

    currying best example

--- index.js
+++ index.js
@@ -2,37 +2,26 @@ function log(level, date, message) {
   console.log(`[${level}] :: ${date} :: ${message}`)
 }
 
-// Callable
-// log("DEBUG", new Date(), "This is a debug message")
-// log("DEBUG", new Date(), "This is a debug message")
-// log("DEBUG", new Date(), "This is a debug message")
-// log("DEBUG", new Date(), "This is a debug message")
-// log("DEBUG", new Date(), "This is a debug message")
-// log("DEBUG", new Date(), "This is a debug message")
-// log("DEBUG", new Date(), "This is a debug message")
-// log("DEBUG", new Date(), "This is a debug message")
-// log("DEBUG", new Date(), "This is a debug message")
-
-// Currying
-// log("DEBUG")(new Date())("This is a debug message")
+// log("DEBUG", new Date(), "This is a debug message");
 
 
 function curry(log) {
-  return function curried (...args) {
+  return function curried(...args) {
     if(args.length >= log.length) {
-      log.apply(this, args);
+      log.apply(this, args)
     } else {
-      return function (...args2) {
+      return function(...args2) {
         return curried.apply(this, args.concat(args2))
       }
     }
-  } 
+  }
 }
 
-const rN = curry(log)("DEBUG")(new Date())
+const cLog = curry(log);
 
-rN("This is a debug message")
-rN("This is a debug message 1")
-rN("This is a debug message 2")
-rN("This is a debug message 3")
-rN("This is a debug message 4")
+const debugNow = cLog("INFO")(new Date())("This is a debug message")
+
+// debugNow(new Date(),"This is a debug message")
+
+
+
