87dd9d130dab5c270ad24770e9bb132b7c400458
kartikmukati <kartikmukati2021@gmail.com>
2024-12-17 13:57:30 +0530

    promise.all()

--- index.js
+++ index.js
@@ -1,105 +1,46 @@
-const STATE = {
-  PENDING: 'PENDING',
-  FULFILLED: 'FULFILLED',
-  REJECTED: 'REJECTED'
-}
+const promise1 = Promise.resolve(5);
 
-class MyPromise {
-  constructor(callback) {
-    // ? it can be pending, fulfilled or rejected
-    this.state = STATE.PENDING;
-    // ? stores callback function like then, catch, finally
-    this.handlers = []; 
-    // ? resolved or rejected value
-    this.value = undefined;
+const promise2 = 2022;
 
-    try {
-      callback(this._resolve, this._reject)
-    } catch(err) {
-
-    }
-  }
-
-  _resolve = (value) => {
-   this.updateResult(value, STATE.FULFILLED)
-  }
-
-  _reject = (value) => {
-    this.updateResult(value, STATE.REJECTED)
-  }
-
-  then(onSuccess, onFail) {
-     return new MyPromise((res, rej) => {
-      this.addHandler(
-        {
-          onSuccess: function(value) {
-            if(!onSuccess) {
-              return res(value);
-            }
-            try {
-              return res(onSuccess(value))
-            }catch(err) {
-              return rej(err)
-            }
-          },
-          onFail: function(value) {
-            if(!onFail)
-              return rej(value);
-            try {
-              return res(onFail(value));
-            } catch(err) {
-              return rej(err)
-            }
-          }
-        }
-      )
-    })
-  }
-
-  catch(onFail) {
-    return this.then(null, onFail);
-  }
-
-  updateResult(value, state) {
-    setTimeout(() => {
-
-      if(this.state !== STATE.PENDING)
-        return;
-
-      this.value = value;
-      this.state = state
-
-      // to execute pending handler;
-      this.executeHandlers();
-    },0)
-  }
-
-  addHandler(handler){
-    this.handlers.push(handler)
-    this.executeHandlers();
-  }
-
-  executeHandlers() {
-    if(this.state === STATE.PENDING)
-      return null;
-
-    this.handlers.forEach((handler) => {
-      if(this.state === STATE.FULFILLED) {
-        return handler.onSuccess(this.value)
-      }
-      return handler.onFail(this.value)
-    })
-    this.handlers = [];
-  }
-}
-
-const promise = new MyPromise((resolve, reject) => {
-  // resolve("hello")
-  reject("reject")
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // reject(" Rejected: Try Again")
+    resolve("Hello")
+  },1000)
 })
 
-promise.then((value) => {
-  console.log('then-',value)
-}).catch(err => {
-  console.log(err)
+// Promise.all([promise1, promise2, promise3]).then((res) => {
+//   console.log(res)
+// })
+
+const myAll = (promiseArr) => {
+  let responses = []
+  let count = 0;
+  return new Promise((resolve, reject) => {
+    promiseArr.forEach((promise, index) => {
+
+      if(!(promise instanceof Promise)) {
+        promise = Promise.resolve(promise)
+      }
+
+      promise.then((data) => {
+        responses[index] = data;
+        count++;
+
+        if(promiseArr.length === count) {
+          resolve(responses);
+        }
+      }).catch(err => {
+        reject(err);
+      })
+    })
+  })
+}
+
+Promise.myAll = myAll;
+
+const promiseArr = [promise1,promise2, promise3]
+
+Promise.myAll(promiseArr).then(data => {
+  console.log(data)
 })
